e88793aba4c0d99e8b571daf458b48c36e8289d5
giogimic <matthewatoope@gmail.com>
2026-07-22 18:38:33 -0230

    fix(seed): v1.6.8 fix js-yaml ES module import for Bun database seeding

--- scripts/import-tuxemon-data.ts
+++ scripts/import-tuxemon-data.ts
@@ -9,7 +9,7 @@
 
 import fs from "fs";
 import path from "path";
-import yaml from "js-yaml";
+import * as yaml from "js-yaml";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
