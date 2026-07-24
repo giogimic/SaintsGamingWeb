18948306121d816b57dd914e0a344ef6b594d7d1
giogimic <matthewatoope@gmail.com>
2026-07-24 11:13:21 -0230

    Security: remove all server management scripts from repo, scrub sensitive data from history

--- scripts/deploy-beta.py
+++
@@ -1,7 +0,0 @@
-import paramiko
-import sys
-
-sys.stdout.reconfigure(encoding='utf-8')
-
-# Script placeholder, password removed for security
-print("Use server CLI or securely inject secrets to deploy.")
