5149f811d883e6d0dcf7ebd05285b4b3351dcf6c
giogimic <matthewatoope@gmail.com>
2026-08-26 00:33:19 -0230

    v2.1.459-42: Fix TypeScript error by adding imagewidth and imageheight to TilesetMeta

--- src/shared/game/types/map.ts
+++ src/shared/game/types/map.ts
@@ -36,6 +36,8 @@ export interface TilesetMeta {
   columns: number;
   tilewidth: number;
   tileheight: number;
+  imagewidth?: number;
+  imageheight?: number;
 }
 
 export interface MapConnection {
