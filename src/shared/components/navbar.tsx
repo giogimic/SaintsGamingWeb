589fdad6ceffa44def34eb1bb3f4461a5bc98a81
giogimic <matthewatoope@gmail.com>
2026-09-02 04:20:41 -0230

    fix(setup): auto-sanitize docker-compose args in setup.sh and update.sh (v2.1.656)

--- src/shared/components/navbar.tsx
+++ src/shared/components/navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar({
   dbPermissionLevel,
   discordLink,
   showUcpLink = false,
-  siteVersion = "v2.1.655",
+  siteVersion = "v2.1.656",
   gameTitle = "The Lobby",
 }: {
   session: any | null;
