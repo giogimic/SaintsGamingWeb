5fb26fedc6078cd4c062c9d2732598ca21ad82c7
giogimic <matthewatoope@gmail.com>
2026-09-02 04:10:19 -0230

    chore(setup): remove sqlite option from setup script and standardize MariaDB/MySQL (v2.1.653)

--- src/shared/components/global-bottom-bar.tsx
+++ src/shared/components/global-bottom-bar.tsx
@@ -80,7 +80,7 @@ interface ClientErrorLog {
 
 export function GlobalBottomBar({
   dbPermissionLevel,
-  siteVersion = "v2.1.652",
+  siteVersion = "v2.1.653",
 }: {
   dbPermissionLevel?: number;
   siteVersion?: string;
