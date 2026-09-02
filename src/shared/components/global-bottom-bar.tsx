4022597f926b9788c5d26842185a60938d62da0c
giogimic <matthewatoope@gmail.com>
2026-09-02 08:36:26 -0230

    chore: bump version to v2.1.670

--- src/shared/components/global-bottom-bar.tsx
+++ src/shared/components/global-bottom-bar.tsx
@@ -80,7 +80,7 @@ interface ClientErrorLog {
 
 export function GlobalBottomBar({
   dbPermissionLevel,
-  siteVersion = "v2.1.669",
+  siteVersion = "v2.1.670",
 }: {
   dbPermissionLevel?: number;
   siteVersion?: string;
