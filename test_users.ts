51bd50d449fa8d1d5436edee8a79acf67b61d4b3
giogimic <matthewatoope@gmail.com>
2026-09-19 00:10:28 -0230

    Remove temporary test scripts breaking the build

--- test_users.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany({ select: { username: true, displayName: true, permissionLevel: true, roleId: true } });
-  console.log(users);
-  process.exit(0);
-}
-main();
